7b0bb9c41a76861e12fee8329f9cb58ddec08b2a
maukku <maurorisso17@gmail.com>
2022-12-19 15:38:23 +0200

    error handling added

--- App.js
+++ App.js
@@ -23,20 +23,37 @@ const styles = StyleSheet.create({
     fontSize: 18,
     marginBottom: 5,
   },
+  error: {
+    color: 'red',
+    fontSize: 16,
+    marginBottom: 10,
+  },
 });
+
 const App = () => {
   const [search, setSearch] = useState('');
   const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleSearch = async () => {
     try {
       const response = await fetch(`https://api.github.com/users/${search}`);
+      if (!response.ok) {
+        let errorMessage = response.statusText;
+        if (response.status === 404) {
+          errorMessage = 'User not found';
+        }
+        throw new Error(errorMessage);
+      }
       const userData = await response.json();
       setUser(userData);
+      setError(null);
     } catch (error) {
       console.error(error);
+      setError(error.message);
     }
   };
+  
 
   return (
     <View style={styles.container}>
@@ -47,6 +64,7 @@ const App = () => {
         placeholder="Enter a GitHub username"
       />
       <Button title="Search" onPress={handleSearch} />
+      {error && <Text style={styles.error}>Error: {error}</Text>}
       {user && (
         <View style={styles.userContainer}>
           <Text style={styles.userInfo}>Username: {user.login}</Text>
